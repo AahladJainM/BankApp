497750810baeb731e081c09e4f29d76e451662e8
AahladJainM <79209969+AahladJainM@users.noreply.github.com>
2021-03-14 18:51:27 +0530

    Add files via upload

---
+++ BankingApplication/src/bankingApp/CentralBank.java
@@ -0,0 +1,10 @@
+package bankingApp;
+
+public class CentralBank {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Account Ramesh = new Account("Ramesh", "CB00001");
+		Ramesh.showMenu();
+	}
+}
